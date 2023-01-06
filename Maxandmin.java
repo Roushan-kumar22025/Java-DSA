088de942acf8d0a23074caa699c0e4de7f8c9d34
Roushan Kumar <122087348+Roushan-kumar22025@users.noreply.github.com>
2023-01-06 17:56:42 +0000

    - Modify Maxandmin.java to fix a bug in the calculation of the minimum value

--- Maxandmin.java
+++ Maxandmin.java
@@ -14,7 +14,7 @@ public class Maxandmin {
           int max = a[0];
           int min = a[0];
          
-           for(int i=0; i<a.length; i++) {
+           for(int i=1; i<a.length; i++) {
                if(a[i] < min) {
                    min = a[i];
                }
