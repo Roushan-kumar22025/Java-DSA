7916d70ee2314591ef1500248f305bd82355ee28
Roushan Kumar <122087348+Roushan-kumar22025@users.noreply.github.com>
2023-01-09 23:48:24 +0530

    Update Reversearray.java

--- Reversearray.java
+++ Reversearray.java
@@ -1,19 +1,16 @@
-// Importing Essential Libraries
-import java.util.*;
-
-// Driver Code
-public class Reversearray {
-    public static void main(String[] args) {
-        // Input array
-        int i;
-        int[] array = {1, 2, 3, 4, 5};
-        System.out.println("Input Array :" + Arrays.toString(array));
-
-        // Printing the elements of the array in Reverse Order
-        System.out.print("Reversed Array : [");
-        for(i = array.length - 1; i >= 1; i--) {
-            System.out.print(array[i] + ", ");
-        }
-        System.out.print(array[i] + "]");
-    }
-}
+public class Reversearray {  
+    public static void main(String[] args) {  
+        //Initialize array  
+        int [] arr = new int [] {1, 2, 3, 4, 5};  
+        System.out.println("Original array: ");  
+        for (int i = 0; i < arr.length; i++) {  
+            System.out.print(arr[i] + " ");  
+        }  
+        System.out.println();  
+        System.out.println("Array in reverse order: ");  
+        //Loop through the array in reverse order  
+        for (int i = arr.length-1; i >= 0; i--) {  
+            System.out.print(arr[i] + " ");  
+        }  
+    }  
+}  
